4262cbf25dcf34bba8ba720e84a5bd2e4d95c6b3
Thiago <thiagocarv4@gmail.com>
2026-05-01 19:12:06 -0300

    feat: gerando tabela de job

---
+++ src/main/java/br/com/thiago/gestao_vagas/modules/company/entities/JobEntity.java
@@ -0,0 +1,44 @@
+package br.com.thiago.gestao_vagas.modules.company.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "job")
+public class JobEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  private String description;
+  private String level;
+  private String benefits;
+
+  @ManyToOne
+  @JoinColumn(name = "id_company")
+  private CompanyEntity company;
+
+  @Column(name = "id_company")
+  private UUID idCompany;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+}
