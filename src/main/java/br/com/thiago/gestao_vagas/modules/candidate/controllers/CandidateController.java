6e36e9120d1fd1ae6f1919cc91e213ee4ed02aa7
Thiago <thiagocarv4@gmail.com>
2026-05-01 14:12:49 -0300

    feat: create candidate

---
+++ src/main/java/br/com/thiago/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,19 @@
+package br.com.thiago.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.thiago.gestao_vagas.modules.candidate.CandidateEntity;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+  @PostMapping
+  public ResponseEntity<Object> create(@RequestBody CandidateEntity candidateEntity) {
+    return ResponseEntity.ok().body(candidateEntity);
+  }
+}
